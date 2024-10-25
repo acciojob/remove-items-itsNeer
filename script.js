1bca47ebeb83fa31c48dbafe9cfc3228c899ac35
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-10-25 23:19:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let select = document.querySelector("#colorSelect");
+let button = document.querySelector("form input");
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    select.remove(select.selectedIndex);
+})
